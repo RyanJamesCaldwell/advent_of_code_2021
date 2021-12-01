4cc998b38fe0ab06b777c5560308bf72722f9b66
Ryan Caldwell <ryan.caldwell@joinroot.com>
2021-12-01 16:10:28 -0500

    Run Go formatter
    gofmt -w .

--- Day1/Day1.go
+++ Day1/Day1.go
@@ -1,65 +1,64 @@
 package Day1
 
-import(
-  f "adventOfCode/fileReader"
-  "fmt"
-  "path/filepath"
-  "strings"
-  "strconv"
+import (
+	f "adventOfCode/fileReader"
+	"fmt"
+	"path/filepath"
+	"strconv"
+	"strings"
 )
 
 func Header() {
-  fmt.Println("AoC Day1 Puzzle Solution")
+	fmt.Println("AoC Day1 Puzzle Solution")
 }
 
 func Solve() {
-  absolutePath, _ := filepath.Abs("./Day1/input.txt")
-  strDataSlice := strings.Split(f.ReadFile(absolutePath), "\n")
+	absolutePath, _ := filepath.Abs("./Day1/input.txt")
+	strDataSlice := strings.Split(f.ReadFile(absolutePath), "\n")
 
-  intDataSlice := make([]int, len(strDataSlice))
-  for idx, val := range strDataSlice {
-    intValue, _ := strconv.Atoi(val)
-    intDataSlice[idx] = intValue
-  }
+	intDataSlice := make([]int, len(strDataSlice))
+	for idx, val := range strDataSlice {
+		intValue, _ := strconv.Atoi(val)
+		intDataSlice[idx] = intValue
+	}
 
-  fmt.Println("Part 1 Solution:", part1(intDataSlice))
-  fmt.Println("Part 2 Solution:", part2(intDataSlice))
+	fmt.Println("Part 1 Solution:", part1(intDataSlice))
+	fmt.Println("Part 2 Solution:", part2(intDataSlice))
 }
 
 func part1(dataSlice []int) int {
-  count := 0
-  for idx, value := range dataSlice {
-    if idx == 0 {
-      continue
-    }
+	count := 0
+	for idx, value := range dataSlice {
+		if idx == 0 {
+			continue
+		}
 
-    if value > dataSlice[idx - 1] {
-      count++
-    }
-  }
+		if value > dataSlice[idx-1] {
+			count++
+		}
+	}
 
-  return count
+	return count
 }
 
 func part2(dataSlice []int) int {
-  count := 0
+	count := 0
 
-  summedWindows := make([]int, len(dataSlice) - 2) // -2 to account for first block of three
+	summedWindows := make([]int, len(dataSlice)-2) // -2 to account for first block of three
 
-  for i := 2; i < len(dataSlice); i++ {
-    summedWindows[count] = sumSlice(dataSlice[i-2:i+1])
-    count++
-  }
+	for i := 2; i < len(dataSlice); i++ {
+		summedWindows[count] = sumSlice(dataSlice[i-2 : i+1])
+		count++
+	}
 
-  return part1(summedWindows)
+	return part1(summedWindows)
 }
 
 func sumSlice(slice []int) int {
-  result := 0
-  for _, val := range slice {
-    result += val
-  }
+	result := 0
+	for _, val := range slice {
+		result += val
+	}
 
-  return result
+	return result
 }
-
--- fileReader/fileReader.go
+++ fileReader/fileReader.go
@@ -1,19 +1,18 @@
 package fileReader
 
-import(
-  "io/ioutil"
+import (
+	"io/ioutil"
 )
 
 func checkErr(err error) {
-  if err != nil {
-    panic(err)
-  }
+	if err != nil {
+		panic(err)
+	}
 }
 
 func ReadFile(filePath string) string {
-  data, err := ioutil.ReadFile(filePath)
-  checkErr(err)
+	data, err := ioutil.ReadFile(filePath)
+	checkErr(err)
 
-  return string(data)
+	return string(data)
 }
-
