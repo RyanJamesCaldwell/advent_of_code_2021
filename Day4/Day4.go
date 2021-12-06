6406efc0cc604ac7b8f754283da7e55eff6f04f1
Ryan Caldwell <ryan.caldwell@joinroot.com>
2021-12-05 21:26:41 -0500

    Day 4 part 1 solution

--- Day4/Day4.go
+++ Day4/Day4.go
@@ -9,96 +9,185 @@ import (
 )
 
 func Header() {
-  fmt.Println("AoC Day4 Puzzle Solution")
+	fmt.Println("AoC Day4 Puzzle Solution")
 }
 
 func Solve() {
-	absolutePath, _ := filepath.Abs("./Day4/sample_input.txt")
+	absolutePath, _ := filepath.Abs("./Day4/input.txt")
 	strDataSlice := strings.Split(f.ReadFile(absolutePath), "\n")
 
-  fmt.Println("Part 1 solution:", part1(strDataSlice))
-  fmt.Println("Part 2 solution:", part2(strDataSlice))
+	fmt.Println("Part 1 solution:", part1(strDataSlice))
+	fmt.Println("Part 2 solution:", part2(strDataSlice))
 }
 
 func part1(data []string) int {
-  drawnNumbers := buildDrawnNumbers(data)
-  boards := buildBoardsFromRawBoardData(data[2:])
+	drawnNumbers := buildDrawnNumbers(data)
+	boards := buildBoardsFromRawBoardData(data[2:])
+	var firstWinningBoard [][]map[string]int
+	var numberCausingWin int
 
-  fmt.Println("Drawn numbers:", drawnNumbers)
-  fmt.Println("Boards:", boards)
+out:
+	for _, currentNumber := range drawnNumbers {
+		for idx, _ := range boards {
+			currentBoard := boards[idx]
 
-  printBoard(boards[0])
+			for rowIdx, rowVal := range currentBoard {
+				for colIdx, _ := range rowVal {
+					if currentBoard[rowIdx][colIdx]["value"] == currentNumber {
+						currentBoard[rowIdx][colIdx]["marked"] = 1
+						if boardIsWinner(currentBoard) {
+							firstWinningBoard = currentBoard
+							numberCausingWin = currentNumber
+							break out
+						}
+					}
+				}
+			}
+		}
+	}
 
-  return 0
+	sumUnmarkedNumbers := sumOfUnmarkedNumbers(firstWinningBoard)
+
+	return sumUnmarkedNumbers * numberCausingWin
 }
 
 func part2(data []string) int {
-  return 0
+	return 0
+}
+
+func sumOfUnmarkedNumbers(board [][]map[string]int) int {
+	sum := 0
+
+	for rowIdx, _ := range board {
+		for colIdx, _ := range board[rowIdx] {
+			if board[rowIdx][colIdx]["marked"] == 0 {
+				sum += board[rowIdx][colIdx]["value"]
+			}
+		}
+	}
+
+	return sum
+}
+
+func boardIsWinner(board [][]map[string]int) bool {
+	boardIsWinner := false
+
+	winsByRows := boardWinsByRows(board)
+	winsByCols := boardWinsByCols(board)
+
+	if winsByRows == true || winsByCols == true {
+		boardIsWinner = true
+	}
+
+	return boardIsWinner
+}
+
+func boardWinsByRows(board [][]map[string]int) bool {
+	winsByRows := false
+
+	for rowIdx, _ := range board {
+		allRowNumbersMarked := allNumbersMarked(board[rowIdx])
+		if allRowNumbersMarked == true {
+			winsByRows = true
+			break
+		}
+	}
+
+	return winsByRows
+}
+
+func boardWinsByCols(board [][]map[string]int) bool {
+	transposedBoard := transpose(board)
+
+	return boardWinsByRows(transposedBoard)
+}
+
+func transpose(a [][]map[string]int) [][]map[string]int {
+	newArr := make([][]map[string]int, len(a))
+	for i := 0; i < len(a); i++ {
+		for j := 0; j < len(a[0]); j++ {
+			newArr[j] = append(newArr[j], a[i][j])
+		}
+	}
+	return newArr
+}
+
+func allNumbersMarked(numbersMap []map[string]int) bool {
+	allMarkedCount := 5
+	actualMarkedCount := 0
+
+	for idx, _ := range numbersMap {
+		if numbersMap[idx]["marked"] == 1 {
+			actualMarkedCount++
+		}
+	}
+
+	return actualMarkedCount == allMarkedCount
 }
 
 func printBoard(board [][]map[string]int) {
-  fmt.Println("Board")
-  fmt.Println("==========")
+	fmt.Println("Board")
+	fmt.Println("==========")
 
-  for _, rowValue := range board {
-    rowSlice := []string{}
+	for _, rowValue := range board {
+		rowSlice := []string{}
 
-    for _, col := range rowValue {
-      str := strconv.Itoa(col["value"])
-      rowSlice = append(rowSlice, str)
-    }
+		for _, col := range rowValue {
+			str := strconv.Itoa(col["value"])
+			rowSlice = append(rowSlice, str)
+		}
 
-    fmt.Println(strings.Join(rowSlice, ", "))
-  }
+		fmt.Println(strings.Join(rowSlice, ", "))
+	}
+	fmt.Println("")
 }
 
 func buildBoardsFromRawBoardData(rawBoardData []string) [][][]map[string]int {
-  var boards [][][]map[string]int
+	var boards [][][]map[string]int
 
-  for i := 0; i < len(rawBoardData) - 1; i += 5 {
-    if len(rawBoardData[i]) == 0 {
-      i++
-    }
-    board := buildBoard(rawBoardData[i:i+5])
-    boards = append(boards, board)
-  }
+	for i := 0; i < len(rawBoardData)-1; i += 5 {
+		if len(rawBoardData[i]) == 0 {
+			i++
+		}
+		board := buildBoard(rawBoardData[i : i+5])
+		boards = append(boards, board)
+	}
 
-  return boards
+	return boards
 }
 
 func buildDrawnNumbers(input []string) []int {
-  strInputSlice := strings.Split(input[0], ",")
-  intInputSlice := make([]int, len(strInputSlice))
+	strInputSlice := strings.Split(input[0], ",")
+	intInputSlice := make([]int, len(strInputSlice))
 
-  for idx, val := range strInputSlice {
-    integerVal, _ := strconv.Atoi(val)
-    intInputSlice[idx] = integerVal
-  }
+	for idx, val := range strInputSlice {
+		integerVal, _ := strconv.Atoi(val)
+		intInputSlice[idx] = integerVal
+	}
 
-  return intInputSlice
+	return intInputSlice
 }
 
 func buildBoard(input []string) [][]map[string]int {
-  result := make([][]map[string]int, 5)
+	result := make([][]map[string]int, 5)
 
-  for i := 0; i < 5; i++ {
-    result[i] = make([]map[string]int, 5)
-  }
+	for i := 0; i < 5; i++ {
+		result[i] = make([]map[string]int, 5)
+	}
 
-  for rowIdx, rowValue := range input {
-    fixedRow := strings.TrimSpace(strings.Replace(rowValue, "  ", " ", -1))
+	for rowIdx, rowValue := range input {
+		fixedRow := strings.TrimSpace(strings.Replace(rowValue, "  ", " ", -1))
 
-    for colIdx, colValue := range strings.Split(fixedRow, " ") {
-      boardPositionMap := make(map[string]int)
-      intValue, _ := strconv.Atoi(colValue)
+		for colIdx, colValue := range strings.Split(fixedRow, " ") {
+			boardPositionMap := make(map[string]int)
+			intValue, _ := strconv.Atoi(colValue)
 
-      boardPositionMap["value"] = intValue
-      boardPositionMap["occupied"] = 0
+			boardPositionMap["value"] = intValue
+			boardPositionMap["marked"] = 0
 
-      result[rowIdx][colIdx] = boardPositionMap
-    }
-  }
+			result[rowIdx][colIdx] = boardPositionMap
+		}
+	}
 
-  return result
+	return result
 }
-
